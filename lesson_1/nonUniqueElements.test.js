f18b56a1a0aaa08f03c3fec572631fe08bbfcbd0
ilmarch1 <ilmarch1@mts.ru>
2024-09-19 15:47:45 +0300

    Add new test

--- lesson_1/nonUniqueElements.test.js
+++ lesson_1/nonUniqueElements.test.js
@@ -6,6 +6,7 @@ test('returns non unique elements', () => {
   expect(nonUniqueElements([1, 2, 3, 4, 5])).toEqual([]);
   expect(nonUniqueElements([5, 5, 5, 5, 5])).toEqual([5, 5, 5, 5, 5]);
   expect(nonUniqueElements([10, 9, 10, 10, 9, 8])).toEqual([10, 9, 10, 10, 9]);
+  expect(nonUniqueElements([])).toEqual([]);
 })
 
 test('throw error with non array arg', () => {

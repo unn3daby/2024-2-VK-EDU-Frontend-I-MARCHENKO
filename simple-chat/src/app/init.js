a6084be46118b5f75b270437f013b8b9dfdc6ed9
ilmarch1 <ilmarch1@mts.ru>
2024-10-21 18:07:02 +0300

    Add anims

--- simple-chat/src/app/init.js
+++ simple-chat/src/app/init.js
@@ -2,6 +2,7 @@ import '@mdi/font/css/materialdesignicons.css';
 import '@/style/index.scss';
 
 import { ChatPageRenderer } from './chatPageRenderer.js';
+import { initAnims } from './anims.js';
 import { Chat } from './chatList.js';
 import { Form } from './form.js';
 
@@ -14,6 +15,8 @@ function init() {
   const chat = new Chat('.chat__body', '.button__add', chatRenderer);
 
   new Form('.dialog', '#chat__input', chat, chatRenderer);
+
+  initAnims('.burger', '.chat');
 }
 
 document.addEventListener('DOMContentLoaded', init);
